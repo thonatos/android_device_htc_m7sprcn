d965bf92706b0f7fc88b6e00f5e340fec1a79886
Thonatos.Yang <thonatos.yang@gmail.com>
2014-09-17 10:50:42 +0800

    m7sprcn : Move source from CM11 directory , so we can just sync by using 'repo sync'

---
+++ cm.mk
@@ -0,0 +1,14 @@
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/htc/m7sprcn/full_m7sprcn.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=sprint_wwe \
+    BUILD_ID=KOT49H \
+    BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:4.4.2/KOT49H/337390.3:user/release-keys" \
+    PRIVATE_BUILD_DESC="5.03.651.3 CL337390 release-keys"
+
+PRODUCT_NAME := cm_m7sprcn
+PRODUCT_DEVICE := m7sprcn
